39b36f45409b01d52b36ffb8247a5668b9f7810d
Julio Urrego <jut9912@gmail.com>
2023-01-20 13:54:26 -0500

    kick and ban commands functional

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,6 +1,52 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js')
+const { Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js')
 
 module.exports = {
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interacion 
+     */
+
+    callback: async (client,interacion) => {
+        const targetUserId = interacion.options.get('target-user').value
+        const reason = interacion.options.get('reason')?.value ||'No reason';
+
+        const targetUser = await interacion.guild.members.fetch(targetUserId);
+
+        if(!targetUser){
+            await interacion.editReply(`The user doesn't exist in this server`)
+            return
+        }
+
+        if(targetUserId===interacion.guild.ownerId){
+            await interacion.editReply(`You can't ban the server owner`)
+            return
+        }
+
+        const targetUserRolePosition = targetUser.roles.highest.position
+        const requestUserRolePosition = interacion.member.roles.highest.targetUserRolePosition
+        const botRolePosition = interacion.guild.members.me.roles.highest.position
+
+        if(targetUserRolePosition >= requestUserRolePosition){
+            await interacion.editReply(`You can't ban a user with same or higher role than you`)
+            return
+        }
+        
+        if(targetUserRolePosition >= botRolePosition){
+            await interacion.editReply(`You can't ban a user with same or higher role than the bot`)
+            return
+        }
+
+        //baning user
+        try {
+            await targetUser.ban({reason})
+            await interacion.editReply(`User ${targetUser} was banned\nReason: ${reason}`)
+        } catch (error) {
+            console.log('ERROR in ban command:')
+            console.error(error)
+        }
+
+    },
 	name: 'ban',
     description: 'bans a memeber from a server',
     options: [
@@ -16,9 +62,6 @@ module.exports = {
             type: ApplicationCommandOptionType.String,
         }
     ],
-    permissionsRequired: [PermissionFlagsBits.Administrator],
-    botPermissions: [PermissionFlagsBits.Administrator],
-    callback: (client,interacion) => {
-        interacion.reply(`user banned`)
-    }
+    permissionsRequired: [PermissionFlagsBits.BanMembers],
+    botPermissions: [PermissionFlagsBits.BanMembers]
 };
--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,8 +1,54 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js')
+const { Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js')
 
 module.exports = {
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interacion 
+     */
+
+    callback: async (client,interacion) => {
+        const targetUserId = interacion.options.get('target-user').value
+        const reason = interacion.options.get('reason')?.value ||'No reason';
+
+        const targetUser = await interacion.guild.members.fetch(targetUserId);
+
+        if(!targetUser){
+            await interacion.editReply(`The user doesn't exist in this server`)
+            return
+        }
+
+        if(targetUserId===interacion.guild.ownerId){
+            await interacion.editReply(`You can't kick the server owner`)
+            return
+        }
+
+        const targetUserRolePosition = targetUser.roles.highest.position
+        const requestUserRolePosition = interacion.member.roles.highest.targetUserRolePosition
+        const botRolePosition = interacion.guild.members.me.roles.highest.position
+
+        if(targetUserRolePosition >= requestUserRolePosition){
+            await interacion.editReply(`You can't kick a user with same or higher role than you`)
+            return
+        }
+        
+        if(targetUserRolePosition >= botRolePosition){
+            await interacion.editReply(`You can't kick a user with same or higher role than the bot`)
+            return
+        }
+
+        //kicking user
+        try {
+            await targetUser.kick({reason})
+            await interacion.editReply(`User ${targetUser} was kicked\nReason: ${reason}`)
+        } catch (error) {
+            console.log('ERROR in kick command:')
+            console.error(error)
+        }
+
+    },
 	name: 'kick',
-    description: 'bans a memeber from a server',
+    description: 'kicks a memeber from a server',
     options: [
         {
             name: 'target-user',
@@ -16,9 +62,6 @@ module.exports = {
             type: ApplicationCommandOptionType.String,
         }
     ],
-    permissionsRequired: [PermissionFlagsBits.Administrator],
-    botPermissions: [PermissionFlagsBits.Administrator],
-    callback: (client,interacion) => {
-        interacion.reply(`user kicked`)
-    }
+    permissionsRequired: [PermissionFlagsBits.kickMembers],
+    botPermissions: [PermissionFlagsBits.kickMembers]
 };
